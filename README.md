d4bae8fc4db21dea9f558245dedb250e7ceebee9
Tomomi Imura <tomomi.i@gmail.com>
2015-05-04 16:40:02 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Temperature Sensor Display Demo
+
+
+This demo showcases a data visualization using the actual data sent from Atmel's ATWINC-1500 microcontroller.
+
+## Turorial
+
+TBD
+
+## Demo
+
+[Access it now!][demo]
+
+This demo required a physical device running.
+
+If you see a blank graph with purple background, the Atwinc-1500 is not running.
+
+ 
+![Screenshot](screen.png "Screenshot")
+
+
+[demo]: http://pubnub.github.io/atmel-temperature-demo
---
+++ js/app.js
@@ -0,0 +1,156 @@
+/* 
+ *  Atmel WINC-1500 x PubNub Demo
+ *  Displaying the data sent by Atmel WINC-1500 temperature and ambient sensors
+ *  https://github.com/pubnub/atmel-demo
+ * 
+ *  @girlie_mac
+ *  License: MIT
+ */
+
+(function() {
+	'use strict';
+
+	var channel = 'Atmel_Pubnub';
+
+	var bkgd = document.querySelector('.background');
+	var temp = document.querySelector('[data-temp]');
+	var hg = document.querySelector('[data-temp-ui]');
+
+	var firstRun = true;
+	var initTemp = 50;
+
+	var prevTemp = 50;
+
+	function displayTemperature(t) {
+		if(!t) return;
+		if(t === prevTemp) return;
+
+		// Temperature display in text
+
+		temp.textContent = t;
+
+		// Temperature display as thermometer
+
+		var h = (t < 0) ? 0 : t;
+		h = (t <=100) ? t : 100;
+
+		hg.style.width = h + '%';
+
+		// Background toggle
+		var newImg;
+		
+		if(t < 50) {
+			newImg = 'chilly';
+		} else if (t >= 50 && t < 55) {
+			newImg = 'cool';
+		} else if (t >= 55 && t < 60) {
+			newImg = 'moderate';
+		} else if (t >= 60 && t < 65) {
+			newImg = 'comfy';
+		} else if (t >= 65 && t < 75) {
+			newImg = 'warm';
+		} else {
+			newImg = 'hot';
+		}
+
+		bkgd.className = 'background ' + newImg;
+
+		prevTemp = t;		
+	}
+
+	function plotGraph(pubnub) {
+		// Eon
+		eon.chart({
+			channel: channel,
+			padding: {
+				left: 100
+			},
+			flow: {
+				duration: 10
+			},
+			limit: 20,
+			generate: {
+				bindto: '#chart',
+				data: {
+					//x: 'x',
+					//labels: true,
+					colors: {
+						temperature: 'white'
+					}
+				},
+				size: {
+					height: 440
+				},
+				legend: {
+					show: false
+				},
+				tooltip: {
+					show: false
+				},
+				point: {
+					show: false
+				},
+				axis : {
+					y: {
+						// tick: {
+						// 	format: function (d) { return d + '°'; }
+						// 	}
+						// show: false,
+						// label: {
+						// 	text: 'Temperature [F°]',
+						// 	position: 'outer-top'
+						// },
+						max: initTemp + 20,
+						min: initTemp - 10,
+					},
+					x : {
+						//type : 'timeseries',
+						localtime: true,
+						tick: {
+						 	format: '%H:%M:%S',
+						culling: {
+							max: 20
+						}
+						},
+						//show: false
+					}
+				}
+			},
+			connect: function(m) {
+				console.log('eon connected to: '+ m);
+			},
+			message: function(m) {
+				if(firstRun) { 
+					initTemp = m.columns[0][1];
+					firstRun = false;
+				}
+				displayTemperature(m.columns[0][1]);
+			},
+			pubnub: pubnub
+		});
+		
+	}
+
+	function connect() {
+		var pubnub = PUBNUB.init({
+			subscribe_key: 'sub-c-2a73818c-d2d3-11e3-9244-02ee2ddab7fe',
+			publish_key:   'pub-c-6dbe7bfd-6408-430a-add4-85cdfe856b47',
+			//uuid: uuid
+		});
+
+		// pubnub.subscribe({
+		// 	channel: channel,
+		// 	callback: function(m) { 
+		// 		//console.log(m.columns[0]);
+				
+		// 		plotGraph(pubnub, m);
+		// 	}
+		// });
+
+		plotGraph(pubnub);		
+		
+	}
+
+	connect();
+
+})();
